6fbed75f530d73f2a38bcbeb546000eda25838a1
MziouedDounia <d.mzioued0@gmail.com>
2024-12-23 16:01:43 +0100

    TD01 : Ajouter un composant Header dans la page

--- TD-React/src/App.jsx
+++ TD-React/src/App.jsx
@@ -1,16 +1,37 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import logoEmsi from '../public/logo.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 
+// function Header(props) {
+//   let name = "Header"
+//   return (
+//     <header>
+//       <h1>My Header is {props.name}</h1>
+//     </header>
+//   )
+// }
+
+function Header() {
+  return (
+    <header>
+      <img src={logoEmsi} className="logo" alt="Emsi logo"  style={{ width:'380px', height: '120px' }}/>
+      <h1>Introduction à React</h1>
+      <h2>A la découverte des premières notions de React</h2>
+    </header>
+  )
+}
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        
+        {/* <Header></Header> */}
+        {/* <Header name="Header from App"></Header> */}
+        <Header></Header>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
